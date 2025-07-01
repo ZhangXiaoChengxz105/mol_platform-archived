5e5997cbcc38b9872ea4ab6e066dc9f91af66d5f
xiaocheng <xz105@illinois.edu>
2025-07-01 04:53:51 -0500

    final

--- result_analysis/runner.py
+++ result_analysis/runner.py
@@ -322,8 +322,7 @@ if finalres:
 
     # ✅ 执行绘图（如果开启 eval 模式）
     if args.eval:
-        for path in written_files:
-            plot_csv_by_task(path, save_dir=args.plotpath)
+        plot_csv_by_task(output_dir, save_dir=args.plotpath)
 
 else:
     print("⚠️ No results to write.")
